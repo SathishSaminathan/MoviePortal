898bc50d36b9abbfb5d3089be873421267c61fb4
SathishSaminathan <Sathishcse1975@gmail.com>
2020-05-02 10:54:19 +0530

    commit for fixes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -38,31 +38,6 @@ const movieCategories = [
   },
 ];
 
-const dummyData = [
-  {
-    title: 'Anise Aroma Art Bazar',
-    url: 'https://i.ibb.co/hYjK44F/anise-aroma-art-bazaar-277253.jpg',
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    id: 1,
-  },
-  {
-    title: 'Food inside a Bowl',
-    url: 'https://i.ibb.co/JtS24qP/food-inside-bowl-1854037.jpg',
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    id: 2,
-  },
-  {
-    title: 'Vegatable Salad',
-    url:
-      'https://i.ibb.co/JxykVBt/flat-lay-photography-of-vegetable-salad-on-plate-1640777.jpg',
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    id: 3,
-  },
-];
-
 const Home = (props) => {
   const [Videos, setVideos] = useState(null);
   const [GhostList, setGhostList] = useState(null);
